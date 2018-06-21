c3afde0e8a406a2af9bfd68d81c4126ada6575d7
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-06-21 13:32:23 +0200

    conditional vcpkg installation

--- buildscripts/linux-prepare-vcpkg.sh
+++ buildscripts/linux-prepare-vcpkg.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 cd ../../
-git clone https://github.com/Microsoft/vcpkg.git
-cd vcpkg
-./bootstrap-vcpkg.sh 
-./vcpkg install zeromq:x64-linux
 
-cd ..
+if [ -d "$vcpkg" ]; then
+  # check if vcpkg exists and is a valid git clone
+  cd vcpkg
+  if [! git rev-parse --is-inside-work-tree]; then
+      cd ..
+......rm -r -f vcpkg
+  fi
+fi
+
+if [ ! -d "$vcpkg" ]; then
+    # if v
+    git clone https://github.com/Microsoft/vcpkg.git
+    cd vcpkg
+    ./bootstrap-vcpkg.sh 
+    ./vcpkg install zeromq:x64-linux
+    cd ..
+fi
