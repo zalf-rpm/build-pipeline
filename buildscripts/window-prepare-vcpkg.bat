7bd01bde76d39dcb86ee766b7586193ee8fd871f
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-06-25 11:04:39 +0200

    fixed build pipeline environment issues

--- buildscripts/window-prepare-vcpkg.bat
+++ buildscripts/window-prepare-vcpkg.bat
@@ -1,7 +1,6 @@
 cd ../../
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
 call bootstrap-vcpkg.bat
 .\vcpkg install zeromq:x64-windows-static
 .\vcpkg install zeromq:x86-windows-static
