fb2eb5b012e6efbb1014720cd4b718be6cc1b1a2
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-06-25 16:45:36 +0200

    install vcpkg to path

--- buildscripts/linux-prepare-vcpkg.sh
+++ buildscripts/linux-prepare-vcpkg.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-cd ../../
+
+if [ -z "$1" ]; then
+	INSTALL_PATH="../../"
+else 
+	INSTALL_PATH=$1
+fi
+
+cd $INSTALL_PATH
 
 if [ -d "./vcpkg" ]; then
   # check if vcpkg exists and is a valid git clone
@@ -7,20 +14,25 @@ if [ -d "./vcpkg" ]; then
   ISGITREPRO=$(git rev-parse --is-inside-work-tree)
   cd ..
   if [ ! $ISGITREPRO ]; then
-......rm -r -f vcpkg
+	  rm -r -f vcpkg
       echo "deleted invalid git folder"
   else
       echo "vcpkg exists"
   fi
 fi
 
-
+# download from git and install vcpkg
 if [ ! -d "./vcpkg" ]; then
     echo "vcpkg does not exist"
     # create vcpkg if it does not exist
     git clone https://github.com/Microsoft/vcpkg.git
     cd vcpkg
     ./bootstrap-vcpkg.sh 
-    ./vcpkg install zeromq:x64-linux
     cd ..
 fi
+
+cd vcpkg
+# add packages to install here
+# zeromq
+./vcpkg install zeromq:x64-linux
+#boost TBD
