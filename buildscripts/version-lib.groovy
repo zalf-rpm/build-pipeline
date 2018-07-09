a6a9ecc9e9fb8b7f764530351bc530898a043e7b
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-07-09 10:29:22 +0200

    adding reusable elements as groovy lib

---
+++ buildscripts/version-lib.groovy
@@ -0,0 +1,57 @@
+def commitToGit(versionString, relFilePathInCheckout, gitCheckoutFolder)
+{
+    dir(gitCheckoutFolder)
+    {
+        String out = sh returnStdout: true, script: "git commit $relFilePathInCheckout --dry-run -m \"auto commit version file with $versionString\" "
+        print(out)
+        //sh returnStdout: true, script: 'git push origin master'      
+    }
+}
+
+def incrementVersionFile(increaseVersion, filename)
+{
+    def varIncrementParameters = "false false false "
+    def tagMessage = ""
+    def versionStr = ""
+
+    if (increaseVersion == 'PATCH')
+    {
+        varIncrementParameters = "false false true "
+    } else if (increaseVersion == 'MINOR')
+    {
+        varIncrementParameters = "false true false "
+    } else if (increaseVersion == 'MAYOR')
+    {
+        varIncrementParameters = "true false false "
+    }  
+        
+    if (isUnix())
+    { 
+        versionStr = sh returnStdout: true, script: "python build-pipeline/buildscripts/incrementversion.py $filename $varIncrementParameters"
+    }
+    else
+    {
+        versionStr = bat returnStdout: true, script: "python build-pipeline/buildscripts/incrementversion.py $filename $varIncrementParameters"
+    }
+    return versionStr
+}
+
+
+def createGitTag(versionString, message)
+{
+    result = false
+    if (versionString != "")
+    {
+        if (isUnix())
+        { 
+            result = sh returnStatus: true, script: "echo tag -a $versionString -m $message"
+            //sh returnStatus: true, script: "git tag -a $versionString -m $message"
+        }
+        else
+        {
+            result = bat returnStatus: true, script: "echo tag -a $versionString -m $message"
+            //bat returnStatus: true, script: "git tag -a $versionString -m $message"
+        }        
+    }
+    return result
+}
