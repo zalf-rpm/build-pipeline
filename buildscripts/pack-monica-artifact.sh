8ed3afa686261e9d5961b15b23a00e102821ff84
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-07-12 15:56:52 +0200

    fix tar path

--- buildscripts/pack-monica-artifact.sh
+++ buildscripts/pack-monica-artifact.sh
@@ -1,21 +1,23 @@
-VersionNumber=$1
-ArtifactFolder = "artifact/monica$VersionNumber" 
-DeployFolder = "deployartefact"
-rm -rf $DeployFolder
+#!/bin/sh
+
+VERSION_NUMBER=$1
+ARTIFACT_FOLDER = "artifact/monica$VERSION_NUMBER" 
+DEPLOY_FOLDER = "deployartefact"
+rm -rf $DEPLOY_FOLDER
 rm -rf $artifact
-mkdir -p $ArtifactFolder
-mkdir -p $DeployFolder
+mkdir -p $ARTIFACT_FOLDER
+mkdir -p $DEPLOY_FOLDER
 cd monica/_cmake_linux
 
-cp -af monica ../../$ArtifactFolder           
-cp -af monica-run ../../$ArtifactFolder
-cp -af monica-zmq-control-send ../../$ArtifactFolder
-cp -af monica-zmq-run ../../$ArtifactFolder
-cp -af monica_python.so ../../$ArtifactFolder
-cp -af monica-zmq-control ../../$ArtifactFolder
-cp -af monica-zmq-proxy ../../$ArtifactFolder
-cp -af monica-zmq-server ../../$ArtifactFolder
+cp -af monica ../../$ARTIFACT_FOLDER           
+cp -af monica-run ../../$ARTIFACT_FOLDER
+cp -af monica-zmq-control-send ../../$ARTIFACT_FOLDER
+cp -af monica-zmq-run ../../$ARTIFACT_FOLDER
+cp -af monica_python.so ../../$ARTIFACT_FOLDER
+cp -af monica-zmq-control ../../$ARTIFACT_FOLDER
+cp -af monica-zmq-proxy ../../$ARTIFACT_FOLDER
+cp -af monica-zmq-server ../../$ARTIFACT_FOLDER
 
 cd ../../artifact
-tar -cvpzf ../deployartefact/monica$fullVersionStr.tar.gz monica$fullVersionStr --overwrite                   
+tar -cvpzf ../deployartefact/monica$VERSION_NUMBER.tar.gz monica$VERSION_NUMBER --overwrite                   
 cd ..
