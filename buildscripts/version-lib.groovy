54d33bad7bd3bcb70cd4c84e37175d9fce9fae36
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-07-12 11:41:50 +0200

    move echo version to groovy

--- buildscripts/version-lib.groovy
+++ buildscripts/version-lib.groovy
@@ -56,49 +56,6 @@ def incrementVersionFile(increaseVersion, filename)
 }
 
 
-def getBuildNumber(filename)
-{
-    def versionStr = ""
-    if (isUnix())
-    { 
-        versionStr = sh returnStdout: true, script: "python build-pipeline/buildscripts/echoversion.py $filename -build"
-    }
-    else
-    {
-        versionStr = bat returnStdout: true, script: "python build-pipeline/buildscripts/echoversion.py $filename -build"
-    }
-    return versionStr 
-}
-
-def getVersionNumber(filename)
-{
-    def versionStr = ""
-    if (isUnix())
-    { 
-        versionStr = sh returnStdout: true, script: "python build-pipeline/buildscripts/echoversion.py $filename -semantic"
-    }
-    else
-    {
-        versionStr = bat returnStdout: true, script: "python build-pipeline/buildscripts/echoversion.py $filename -semantic"
-    }
-    return versionStr 
-}
-
-def getFullVersionNumber(filename)
-{
-    def versionStr = ""
-    if (isUnix())
-    { 
-        versionStr = sh returnStdout: true, script: "python build-pipeline/buildscripts/echoversion.py $filename"
-    }
-    else
-    {
-        versionStr = bat returnStdout: true, script: "python build-pipeline/buildscripts/echoversion.py $filename"
-    }
-    return versionStr 
-}
-
-
 def createGitTag(versionString, message, credentialsId)
 {
     def result = false
@@ -124,5 +81,53 @@ def createGitTag(versionString, message, credentialsId)
     }
     return result
 }
+
+def getVersionFromVersionFile(buildNumberOnly, semanticVersionOnly, headerfilePath)
+{
+    def result = ""
+    def root = pwd()
+    String fileContents = readFile "${root}/${headerfilePath}"
+    def lines = fileContents.split("\n")
+    int versionMayorLine = 0
+    int versionMinorLine = 0
+    int versionRevisionLine = 0
+    int versionBuildLine = 0
+
+    for (line in lines) 
+    {
+        if (line.startsWith("#define VERSION_MAJOR "))
+        {
+            versionMayorLine = (line - ~'#define VERSION_MAJOR +').toInteger()   
+        }
+        if (line.startsWith("#define VERSION_MINOR "))
+        {
+            versionMinorLine = (line - ~'#define VERSION_MINOR +').toInteger()
+        }
+        if (line.startsWith("#define VERSION_REVISION "))
+        {
+            versionRevisionLine = (line - ~'#define VERSION_REVISION +').toInteger()          
+        }
+        if (line.startsWith("#define VERSION_BUILD "))
+        {
+            versionBuildLine = (line - ~'#define VERSION_BUILD +').toInteger()      
+        }
+    }
+    if (semanticVersionOnly)
+    {
+        result = versionMayorLine + "." + versionMinorLine  + "." + versionRevisionLine
+    }
+    else if (buildNumberOnly)
+    {
+        result = versionBuildLine
+    }
+    else
+    {
+        result = versionMayorLine + "." + versionMinorLine  + "." + versionRevisionLine + "." + versionBuildLine
+    }
+    print(result)
+
+    return result
+}
+
 
 return this
