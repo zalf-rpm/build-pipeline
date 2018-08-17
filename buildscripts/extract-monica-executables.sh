d73efbdf895091af19207d004998ced43996c895
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-17 14:32:20 +0200

    fixing extract

--- buildscripts/extract-monica-executables.sh
+++ buildscripts/extract-monica-executables.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-MONICA_EXECUTABLES = "monica-executables"
-cd ../../artifact
-rm -rf $MONICA_EXECUTABLES
-mkdir $MONICA_EXECUTABLES
-larray=($(ls))
-for i in larray; do
-    if [[$i == monica*.tar.gz]]
-    {
-        echo "extracting $i"
-        tar -vxzf $i ../$MONICA_EXECUTABLES/ --overwrite  
-    }
+MONICA_EXECUTABLES="monica-executables"
+cd artifact
+rm -rf ../$MONICA_EXECUTABLES
+mkdir -p ../$MONICA_EXECUTABLES
+for file in monica_*.tar.gz; do
+    echo "extracting $file"
+    tar -vxzf $file -C ../$MONICA_EXECUTABLES/ --overwrite  
 done
