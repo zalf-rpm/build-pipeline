5ebd554d4a5c640ab57f6aea55dee6458bc59ef8
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-17 13:32:56 +0200

    extract all tar files

--- buildscripts/extract-monica-executables.sh
+++ buildscripts/extract-monica-executables.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
-MONICA_NAME=$1
-mkdir monica-executables
-tar -vxzf $MONICA_NAME.tar.gz /monica-executables/ --overwrite  
+#!/bin/bash
+MONICA_EXECUTABLES = "monica-executables"
+cd ../../artifact
+rm -rf $MONICA_EXECUTABLES
+mkdir $MONICA_EXECUTABLES
+larray=($(ls))
+for i in larray; do
+    if [[$i == monica*.tar.gz]]
+    {
+        echo "extracting $i"
+        tar -vxzf $i ../$MONICA_EXECUTABLES/ --overwrite  
+    }
+done
