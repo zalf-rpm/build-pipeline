fbeb30fee6d7707f389ada99a93b5aa4fb947301
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-20 17:28:17 +0200

    test sleep

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -5,3 +5,4 @@ python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINK
 python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE}
 sleep 10
 ./$MONICA_HOME/testing/CoreConsoleParser -ref /$MONICA_HOME/testing/testreference.csv -totest /$MONICA_HOME/testing/consumer-out.csv
+sleep 15
