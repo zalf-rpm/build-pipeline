a2eaa476cde901383df440ef199cb49d88dba2d7
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-20 16:49:26 +0200

    adding sleep to wait for the startup

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 env
+sleep 10
 python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True &
 python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE}
-sleep 15
+sleep 10
 ./$MONICA_HOME/testing/CoreConsoleParser -ref /$MONICA_HOME/testing/testreference.csv -totest /$MONICA_HOME/testing/consumer-out.csv
