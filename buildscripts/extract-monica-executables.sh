b1ba723bad4b96c24189562c4b9789f421852856
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-20 12:57:28 +0200

    set artifact path

--- buildscripts/extract-monica-executables.sh
+++ buildscripts/extract-monica-executables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 MONICA_EXECUTABLES="monica-executables"
-cd artifact
-rm -rf ../$MONICA_EXECUTABLES
-mkdir -p ../$MONICA_EXECUTABLES
+cd artifact/deployartefact
+rm -rf ../../$MONICA_EXECUTABLES
+mkdir -p ../../$MONICA_EXECUTABLES
 for file in monica_*.tar.gz; do
     echo "extracting $file"
-    tar -vxzf $file -C ../$MONICA_EXECUTABLES/ --overwrite  
+    tar -vxzf $file -C ../../$MONICA_EXECUTABLES/ --overwrite  
 done
