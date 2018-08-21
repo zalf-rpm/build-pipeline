7c5bafec50627be08766f6f7024d135ff1a55ebb
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-21 16:15:21 +0200

    fixing filepaths for test modules

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 env
-sleep 10
-python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True &
+python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True out=/$MONICA_HOME/testing/ &
+sleep 5
 python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE}
-sleep 10
-./$MONICA_HOME/testing/CoreConsoleParser -ref /$MONICA_HOME/testing/testreference.csv -totest /$MONICA_HOME/testing/consumer-out.csv
-sleep 15
+sleep 5
+./$MONICA_HOME/testing/CoreConsoleParser -ref /$MONICA_HOME/testing/testreference.csv -totest /$MONICA_HOME/testing/1.csv
+sleep 5
