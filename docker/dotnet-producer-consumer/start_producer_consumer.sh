aab0c9ef05a91d0b3c5a77dd8f77cf770564bb7c
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-22 10:42:52 +0200

    fixing permissions

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 env
-python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True out=/$MONICA_HOME/testing/ &
+python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True out=$MONICA_HOME/testing/ &
 sleep 5
 python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE}
 sleep 5
-ls -al $MONICA_HOME/testing/CoreConsoleParser
-$MONICA_HOME/testing/CoreConsoleParser -ref /$MONICA_HOME/testing/testreference.csv -totest /$MONICA_HOME/testing/1.csv
+$MONICA_HOME/testing/CoreConsoleParser -ref $MONICA_HOME/testing/testreference.csv -totest $MONICA_HOME/testing/1.csv
 sleep 5
