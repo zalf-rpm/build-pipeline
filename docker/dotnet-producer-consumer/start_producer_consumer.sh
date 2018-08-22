6bf7c76a3ee698f0e63e5d2d635c175eb39c2095
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-08-22 09:57:02 +0200

    changing executable permissions

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -4,5 +4,8 @@ python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINK
 sleep 5
 python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE}
 sleep 5
+ls -al $MONICA_HOME/testing/CoreConsoleParser
+chmod +x $MONICA_HOME/testing/CoreConsoleParser
+ls -al $MONICA_HOME/testing/CoreConsoleParser
 $MONICA_HOME/testing/CoreConsoleParser -ref /$MONICA_HOME/testing/testreference.csv -totest /$MONICA_HOME/testing/1.csv
 sleep 5
