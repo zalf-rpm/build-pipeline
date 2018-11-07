31c74de590415af6953bb5b93e12abadee0db213
sanny <38938782+sannyschulz@users.noreply.github.com>
2018-11-07 16:24:06 +0100

    fixing permission issues on mounting

--- docker/simplace-cluster/simplace_start.sh
+++ docker/simplace-cluster/simplace_start.sh
@@ -1,12 +1,34 @@
-#!/bin/sh
-env
-java -version
+#!/bin/sh -x
+
+mkdir -p $WORKDIR 
+mkdir -p $OUTPUTDIR
+mkdir -p $DATADIR
+
+LOGLEVEL=" -loglevel=ERROR"
+if [ $DEBUG = "true" ]; then
+  env
+  java -version
+  echo "ALL dir"
+  ls -al
+  echo "work dir"
+  ls -al $WORKDIR
+  echo "exe dir"
+  ls -al $EXECDIR
+  echo "out dir"
+  ls -al $OUTPUTDIR
+  echo "data dir"
+  ls -al $DATADIR
+  LOGLEVEL=""
+fi
+
 if [ $TESTRUN = "true" ]; then
   echo "Simplace in Docker: this is a testrun for soulution: $WORKDIR/$SOLUTION"
-  $EXECDIR/simplace run -s=$WORKDIR/$SOLUTION -w=/simplace/SIMPLACE_WORK -o=/output
+  $EXECDIR/simplace run -s=$WORKDIR/$SOLUTION -w=$WORKDIR -o=$OUTPUTDIR
+  echo "output dir"
+  ls -alR $OUTPUTDIR
 else 
   echo "Simplace in Docker - Solution: $WORKDIR/$SOLUTION"
   echo "Simplace in Docker - Project: $WORKDIR/$PROJECT"
   echo "Simplace in Docker - Lines: $LINE_START-$LINE_END"
-  $EXECDIR/simplace run -t=CLUSTER -s=$WORKDIR/$SOLUTION -p=$WORKDIR/$PROJECT -l=$LINE_START-$LINE_END -loglevel=ERROR
+  $EXECDIR/simplace run -t=CLUSTER -s=$WORKDIR/$SOLUTION -p=$WORKDIR/$PROJECT -l=$LINE_START-$LINE_END $LOGLEVEL
 fi
