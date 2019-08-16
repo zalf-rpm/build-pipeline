48f60b6aa10f9a370d671e38a9a4e976b774504e
sanny <38938782+sannyschulz@users.noreply.github.com>
2019-08-16 18:01:31 +0200

    rework for rundeck job

--- hpc/simplace/rundeck_simplace_hpc.sh
+++ hpc/simplace/rundeck_simplace_hpc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #/ usage: start ?user? ?job_name? ?job_exec_id? ?solution_path? ?project_path? ?version? ?lines? ?debug? ?estimated_time? ?used_cpu? ?mount_data? ?mount_work? ?mount_out? ?mount_out_zip? ?mount_project? 
 set -eu
-[[ $# != 15 ]] && {
+[[ $# != 14 ]] && {
   grep '^#/ usage:' <"$0" | cut -c4- >&2 ; exit 2;
 }
 
@@ -17,15 +17,14 @@ JOB_EXEC_ID=$3
 SOLUTION_PATH=$4
 PROJECT_PATH=$5
 VERSION=$6
-LINE_SPLITUPSTR=$7
-DEBUG=$8
-TIME=${9}
-CPU=${10}
-MOUNT_DATA=${11}
-MOUNT_WORK=${12}
-MOUNT_OUT=${13}
-MOUNT_OUT_ZIP=${14}
-MOUNT_PROJECT=${15}
+DEBUG=$7
+TIME=$8
+MOUNT_DATA=$9
+MOUNT_WORK=${10}
+MOUNT_OUT=${11}
+MOUNT_OUT_ZIP=${12}
+MOUNT_PROJECT=${13}
+NODES=${14}
 
 # check if job name is empty 
 if [ -z "$JOB_NAME" ] ; then 
@@ -35,8 +34,12 @@ fi
 #sbatch job name 
 SBATCH_JOB_NAME="simpl_${USER}_${JOB_NAME}_${JOB_EXEC_ID}"
 
+DATE=`date +%Y-%d-%B_%H%M%S`
+MOUNT_OUT_ZIP=$MOUNT_OUT_ZIP/${SBATCH_JOB_NAME}_$DATE
+mkdir $MOUNT_OUT_ZIP
+
 #check if the singularity image exists 
-SINGULARITY_IMAGE=simplace_4.2.1.sif
+SINGULARITY_IMAGE=simplace_${VERSION}.sif
 IMAGE_DIR=~/singularity/simplace
 if [ ! -e ${IMAGE_DIR}/${SINGULARITY_IMAGE} ] ; then
 echo "File '${IMAGE_DIR}/${SINGULARITY_IMAGE}' not found"
@@ -45,8 +48,15 @@ fi
 
 cd ~
 
-#TODO write a program to extract lines, cpu usage, and time estimate
+#extract lines
+LINE_SPLITUPSTR=$( srun singularity run -B \
+$MOUNT_WORK:/simplace/SIMPLACE_WORK,\
+$MOUNT_DATA:/data,\
+$MOUNT_PROJECT:/projects \
+${IMAGE_DIR}/${SINGULARITY_IMAGE} /splitsimplaceproj/splitsimplaceproj /simplace/SIMPLACE_WORK/$PROJECT_PATH 4 $NODES _WORKDIR_?/simplace/SIMPLACE_WORK _PROJECTSDIR_?/projects _DATADIR_?/data)
 
+
+DEPENDENCY=afterany
 IFS=',' # (,) is set as delimiter
 read -ra ADDR <<< "$LINE_SPLITUPSTR" # str is read into an array as tokens separated by IFS
 
@@ -57,7 +67,7 @@ for i in "${ADDR[@]}"; do # access each element of array
     STARTLINE=${SOME[0]}
     ENDLINE=${SOME[1]}
 	#sbatch commands
-	SBATCH_COMMANDS="--job-name=${SBATCH_JOB_NAME}_${i} --time=${TIME} --cpus-per-task=${CPU} -o log/simplace-%j"
+	SBATCH_COMMANDS="--parsable --job-name=${SBATCH_JOB_NAME}_${i} --time=${TIME} --cpus-per-task=40 -o log/simplace-%j"
 	#simplace sbatch script commands
     SIMPLACE_INPUT="${MOUNT_DATA} ${MOUNT_WORK} ${MOUNT_OUT} ${MOUNT_OUT_ZIP} ${MOUNT_PROJECT} ${SOLUTION_PATH} ${PROJECT_PATH} ${IMAGE_DIR}/${SINGULARITY_IMAGE} ${DEBUG} ${STARTLINE} ${ENDLINE} ${SBATCH_JOB_NAME}_${i} false"
     echo "First  $STARTLINE"
@@ -65,5 +75,15 @@ for i in "${ADDR[@]}"; do # access each element of array
     echo "$i"
 
 	echo "sbatch $SBATCH_COMMANDS batch/sbatch_simplace.sh $SIMPLACE_INPUT"
-	sbatch $SBATCH_COMMANDS batch/sbatch_simplace.sh $SIMPLACE_INPUT 
+	BATCHID=$( sbatch $SBATCH_COMMANDS batch/sbatch_simplace.sh $SIMPLACE_INPUT )
+  DEPENDENCY=$DEPENDENCY":"$BATCHID
+  echo "DEPENDENCY: $DEPENDENCY"
 done
+
+# accumulate results into one folder structure
+MOUNT_OUT_ZIP_ACC=${MOUNT_OUT_ZIP}_acc
+mkdir $MOUNT_OUT_ZIP_ACC
+
+echo sbatch --dependency=$DEPENDENCY --job-name=${SBATCH_JOB_NAME}_ACC --time=00:15:00 -o log/simplace-acc%j batch/sbatch_acc_simplace.sh $MOUNT_OUT_ZIP $MOUNT_OUT_ZIP_ACC ${IMAGE_DIR}/${SINGULARITY_IMAGE}
+
+sbatch --dependency=$DEPENDENCY --job-name=${SBATCH_JOB_NAME}_ACC --time=00:15:00 -o log/simplace-acc%j batch/sbatch_acc_simplace.sh $MOUNT_OUT_ZIP $MOUNT_OUT_ZIP_ACC ${IMAGE_DIR}/${SINGULARITY_IMAGE}
---
+++ hpc/simplace/sbatch_acc_simplace.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --partition=compute
+
+OUT_ZIP=$1
+OUT_ZIP_FINAL=$2
+SINGULARITY_IMAGE=$3
+
+OUTDIR=/outputs
+OUTDIR_FINAL=/final
+
+echo "outputs dir: " $OUT_ZIP
+echo "outputs final dir: " $OUT_ZIP_FINAL
+
+srun singularity run -B \
+$OUT_ZIP:$OUTDIR,\
+$OUT_ZIP_FINAL:$OUTDIR_FINAL \
+${SINGULARITY_IMAGE} /accumulate_output/accumulate_output -mergetarball -infolder /outputs -outfolder /final
+
