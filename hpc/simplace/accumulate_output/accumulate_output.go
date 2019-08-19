f1f56a5b2df96631c96a009e60d54b5094b82fa1
sanny <38938782+sannyschulz@users.noreply.github.com>
2019-08-19 17:09:58 +0200

    merge files with same filename

--- hpc/simplace/accumulate_output/accumulate_output.go
+++ hpc/simplace/accumulate_output/accumulate_output.go
@@ -9,7 +9,6 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"path"
 	"path/filepath"
 	"regexp"
 	"sort"
@@ -22,7 +21,7 @@ func main() {
 
 	inFolder := "/home/user/project/out"
 	outFolder := "/home/user/project/sum_out"
-	mergeTarBall := false
+	//mergeTarBall := false
 	argsWithoutExe := os.Args[1:]
 
 	num := len(argsWithoutExe)
@@ -32,9 +31,6 @@ func main() {
 		}
 		if arg == "-outfolder" && i+1 < num {
 			outFolder = argsWithoutExe[i+1]
-		}
-		if arg == "-mergetarball" {
-			mergeTarBall = true
 		}
 	}
 	fmt.Printf("In: %s\n", inFolder)
@@ -48,59 +44,83 @@ func main() {
 	// sort by line numbers
 	fmt.Println("Sorted:")
 	sort.Sort(files)
-	if mergeTarBall {
-		baseFolderName := filepath.Base(inFolder)
-		tarFile := filepath.Join(outFolder, fmt.Sprintf("%s.tar", baseFolderName))
-		err = os.MkdirAll(outFolder, os.ModePerm)
+
+	listing := scanFiles(files, inFolder)
+
+	baseFolderName := filepath.Base(inFolder)
+	tarFileName := filepath.Join(outFolder, fmt.Sprintf("%s.tar", baseFolderName))
+	err = os.MkdirAll(outFolder, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	merge := mergeTarGzipFile(tarFileName)
+	var outFile *os.File
+	var outWriter *tar.Writer
+
+	excludes := make(map[string]bool)
+	for internalPath, listOfTarGz := range listing {
+		if len(listOfTarGz) > 1 {
+			// make dir
+			base := filepath.Base(internalPath)
+			fullFileName := joinPath(outFolder, base)
+			file, err := os.OpenFile(fullFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+			if err != nil {
+				log.Fatalf("Error occured while opening temp file: %s   \n", fullFileName)
+			}
+			for i, fileTarGz := range listOfTarGz {
+				skippFirstLine := i != 0
+				joinInternalFile(fileTarGz, internalPath, skippFirstLine, file)
+			}
+			outFile, outWriter = merge(internalPath, nil, fullFileName)
+			file.Close()
+			err = os.Remove(fullFileName)
+			if err != nil {
+				log.Printf("Failed to delete %v %v", fullFileName, err)
+			}
+			excludes[internalPath] = true
+		}
+	}
+	for _, f := range files {
+		fmt.Println(f.Name())
+		// read file content into output files
+		outFile, outWriter = merge("", excludes, filepath.Join(inFolder, f.Name()))
+	}
+
+	if outWriter != nil && outFile != nil {
+		outWriter.Close()
+		outFile.Close()
+
+		reader, err := os.Open(tarFileName)
 		if err != nil {
 			log.Fatal(err)
 		}
+		tarFileNameBase := filepath.Base(tarFileName)
+		gzTarget := filepath.Join(outFolder, fmt.Sprintf("%s.gz", tarFileNameBase))
 
-		merge := mergeTarGzipFile(tarFile)
-		var outFile *os.File
-		var outWriter *tar.Writer
-		for _, f := range files {
-			fmt.Println(f.Name())
-			// read file content into output files
-			outFile, outWriter = merge(filepath.Join(inFolder, f.Name()))
+		writer, err := os.Create(gzTarget)
+		if err != nil {
+			log.Fatal(err)
 		}
-		if outWriter != nil && outFile != nil {
-			outWriter.Close()
-			outFile.Close()
+		defer writer.Close()
 
-			reader, err := os.Open(tarFile)
-			if err != nil {
-				log.Fatal(err)
-			}
-			tarFileName := filepath.Base(tarFile)
-			gzTarget := filepath.Join(outFolder, fmt.Sprintf("%s.gz", tarFileName))
+		archiver := gzip.NewWriter(writer)
+		archiver.Name = tarFileNameBase
+		defer archiver.Close()
 
-			writer, err := os.Create(gzTarget)
-			if err != nil {
-				log.Fatal(err)
-			}
-			defer writer.Close()
-
-			archiver := gzip.NewWriter(writer)
-			archiver.Name = tarFileName
-			defer archiver.Close()
-
-			_, err = io.Copy(archiver, reader)
-		}
-
-	} else {
-		for _, f := range files {
-			fmt.Println(f.Name())
-			// read file content into output files
-			readTarGzipFile(inFolder+"/"+f.Name(), outFolder)
+		_, err = io.Copy(archiver, reader)
+		reader.Close()
+		err = os.Remove(tarFileName)
+		if err != nil {
+			log.Printf("Failed to delete %v %v", tarFileName, err)
 		}
 	}
 
 }
 
-func readTarGzipFile(filename string, outPath string) {
+func joinInternalFile(tarGzInFile, internalFile string, skippFirstLine bool, tempFile *os.File) {
 
-	f, err := os.Open(filename)
+	f, err := os.Open(tarGzInFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -132,62 +152,40 @@ func readTarGzipFile(filename string, outPath string) {
 			continue
 		case tar.TypeReg:
 			fmt.Printf("Contents of %s %d %d\n", header.Name, header.Mode, header.Size)
-			filePath := header.Name
-			// make dir
-			fullFileName := joinPath(outPath, filePath)
-			fullFilePath := path.Dir(fullFileName)
-			err = os.MkdirAll(fullFilePath, os.ModePerm)
-			if err != nil {
-				log.Fatal(err)
-			}
-			skippFirstLine := false
-			// append content to output file
-			if _, err := os.Stat(fullFileName); err == nil {
-				skippFirstLine = true
-			} else if os.IsNotExist(err) {
-				skippFirstLine = false
-			} else {
-				log.Fatal(err)
-			}
-
-			file, err := os.OpenFile(fullFileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-			if err != nil {
-				log.Fatalf("Error occured while opening result file: %s   \n", filePath)
-			}
-			defer file.Close()
-
-			if skippFirstLine {
-				lineSep := []byte{'\n'}
-				size := 32 * 1024
-				buf := make([]byte, size)
-				for {
-					nr, er := tarReader.Read(buf)
-					if nr > 0 {
-						index := bytes.Index(buf[:nr], lineSep)
-						if index != -1 {
-							if index+1 < nr {
-								_, ew := file.Write(buf[index+1 : nr])
-								if ew != nil {
-									err = ew
-									break
+			if header.Name == internalFile {
+				if skippFirstLine {
+					lineSep := []byte{'\n'}
+					size := 32 * 1024
+					buf := make([]byte, size)
+					for {
+						nr, er := tarReader.Read(buf)
+						if nr > 0 {
+							index := bytes.Index(buf[:nr], lineSep)
+							if index != -1 {
+								if index+1 < nr {
+									_, ew := tempFile.Write(buf[index+1 : nr])
+									if ew != nil {
+										err = ew
+										break
+									}
 								}
+								if _, err := io.CopyBuffer(tempFile, tarReader, buf); err != nil {
+									log.Fatal(err)
+								}
+								break
 							}
-							if _, err := io.CopyBuffer(file, tarReader, buf); err != nil {
-								log.Fatal(err)
+						}
+						if er != nil {
+							if er != io.EOF {
+								err = er
 							}
 							break
 						}
 					}
-					if er != nil {
-						if er != io.EOF {
-							err = er
-						}
-						break
+				} else {
+					if _, err := io.Copy(tempFile, tarReader); err != nil {
+						log.Fatal(err)
 					}
-				}
-			} else {
-				if _, err := io.Copy(file, tarReader); err != nil {
-					log.Fatal(err)
 				}
 			}
 		default:
@@ -202,15 +200,11 @@ func readTarGzipFile(filename string, outPath string) {
 
 }
 
-func mergeTarGzipFile(targetFile string) func(string) (*os.File, *tar.Writer) {
-	tarfile, err := os.Create(targetFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tarball := tar.NewWriter(tarfile)
-	dirLookup := make(map[string]bool)
-	fileLookup := make(map[string]bool)
-	return func(filename string) (*os.File, *tar.Writer) {
+func scanFiles(inputFiles ByLineNumber, inFolder string) (fileListing map[string][]string) {
+	fileListing = make(map[string][]string)
+	for _, ifile := range inputFiles {
+		fmt.Println(ifile.Name())
+		filename := filepath.Join(inFolder, ifile.Name())
 		f, err := os.Open(filename)
 		if err != nil {
 			log.Fatal(err)
@@ -237,22 +231,15 @@ func mergeTarGzipFile(targetFile string) func(string) (*os.File, *tar.Writer) {
 
 			switch header.Typeflag {
 			case tar.TypeDir:
-				fmt.Printf("Directory %s \n", header.Name)
-				if !dirLookup[header.Name] {
-					dirLookup[header.Name] = true
-					tarball.WriteHeader(header)
-				}
+				//fmt.Printf("Directory %s \n", header.Name)
 				continue
 			case tar.TypeReg:
-				fmt.Printf("Contents of %s %d %d\n", header.Name, header.Mode, header.Size)
-				if fileLookup[header.Name] {
-					log.Fatalf("duplicated file '%s' in '%s'\n", header.Name, filename)
-				}
-				fileLookup[header.Name] = true
-				tarball.WriteHeader(header)
+				//fmt.Printf("Contents of %s %d %d\n", header.Name, header.Mode, header.Size)
 
-				if _, err := io.Copy(tarball, tarReader); err != nil {
-					log.Fatal(err)
+				if _, ok := fileListing[header.Name]; ok {
+					fileListing[header.Name] = append(fileListing[header.Name], filename)
+				} else {
+					fileListing[header.Name] = []string{filename}
 				}
 
 			default:
@@ -262,6 +249,88 @@ func mergeTarGzipFile(targetFile string) func(string) (*os.File, *tar.Writer) {
 					"in file",
 					header.Name,
 				)
+			}
+		}
+	}
+	return fileListing
+}
+
+func mergeTarGzipFile(targetFile string) func(string, map[string]bool, string) (*os.File, *tar.Writer) {
+	tarfile, err := os.Create(targetFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tarball := tar.NewWriter(tarfile)
+	//dirLookup := make(map[string]bool)
+	fileLookup := make(map[string]bool)
+	return func(internalFilename string, excludes map[string]bool, filename string) (*os.File, *tar.Writer) {
+		f, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer f.Close()
+		if filepath.Ext(filename) != ".gz" {
+			fileInfo, _ := f.Stat()
+			hdr := &tar.Header{
+				Name: internalFilename,
+				Mode: 0600,
+				Size: fileInfo.Size(),
+			}
+			if err := tarball.WriteHeader(hdr); err != nil {
+				log.Fatal(err)
+			}
+
+			if _, err := io.Copy(tarball, f); err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			// open gzip reader
+			gzf, err := gzip.NewReader(f)
+			if err != nil {
+				log.Fatal(err)
+			}
+			// open tar reader
+			tarReader := tar.NewReader(gzf)
+
+			// read file entries from tar
+			for {
+				header, err := tarReader.Next()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				switch header.Typeflag {
+				case tar.TypeDir:
+					// fmt.Printf("Directory %s \n", header.Name)
+					// if !dirLookup[header.Name] {
+					// 	dirLookup[header.Name] = true
+					// 	tarball.WriteHeader(header)
+					// }
+					continue
+				case tar.TypeReg:
+					fmt.Printf("Contents of %s %d %d\n", header.Name, header.Mode, header.Size)
+					if !excludes[header.Name] {
+						if fileLookup[header.Name] {
+							log.Fatalf("duplicated file '%s' in '%s'\n", header.Name, filename)
+						}
+						fileLookup[header.Name] = true
+						tarball.WriteHeader(header)
+
+						if _, err := io.Copy(tarball, tarReader); err != nil {
+							log.Fatal(err)
+						}
+					}
+				default:
+					fmt.Printf("%s : %c %s %s\n",
+						"Unable to figure out type",
+						header.Typeflag,
+						"in file",
+						header.Name,
+					)
+				}
 			}
 		}
 		return tarfile, tarball
