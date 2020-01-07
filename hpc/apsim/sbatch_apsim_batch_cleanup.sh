2218f4ecdce0abbc4c8977271f2af06521e19963
sanny <38938782+sannyschulz@users.noreply.github.com>
2020-01-07 13:37:17 +0100

    fix to many files error with mv

--- hpc/apsim/sbatch_apsim_batch_cleanup.sh
+++ hpc/apsim/sbatch_apsim_batch_cleanup.sh
@@ -10,8 +10,13 @@ APSIM_OUT_FOLDER=$3
 echo "apsim temp dir: " $APSIM_TEMP
 rm -rf $APSIM_TEMP
 
-mv ${APSIM_SIM_FOLDER}/*.out ${APSIM_OUT_FOLDER}
-mv ${APSIM_SIM_FOLDER}/*.sum ${APSIM_OUT_FOLDER}
+cd ${APSIM_SIM_FOLDER}
+for f in *.out; do 
+    mv "$f" ${APSIM_OUT_FOLDER}
+done
+for f in *.sum; do 
+    mv "$f" ${APSIM_OUT_FOLDER}
+done
 
 ZIPNAME=`basename ${APSIM_OUT_FOLDER}`
 cd ${APSIM_OUT_FOLDER}/..
