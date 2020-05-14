2c5b0efa48553f32b75b1cd2e0d5b8e07f1241e9
sanny <38938782+sannyschulz@users.noreply.github.com>
2020-05-14 20:26:34 +0200

    export tested results

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -4,6 +4,8 @@ python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINK
 sleep 5
 python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE}
 sleep 5
+mkdir -p /out
+cp $MONICA_HOME/testing/1.csv /out
 $MONICA_HOME/testing/CoreConsoleParser -ref $MONICA_HOME/testing/testreference.csv -totest $MONICA_HOME/testing/1.csv
 return $?
 
