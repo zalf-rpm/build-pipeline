0081750127e179958d9d913570c0eabd100dedc5
sanny <38938782+sannyschulz@users.noreply.github.com>
2020-05-15 14:38:34 +0200

    fix path

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 env
 mkdir -p /out
-python3 -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True out=$MONICA_HOME/testing/ &
+python3 -u $MONICA_HOME/Examples/Hohenfinow2/python3/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True out=$MONICA_HOME/testing/ &
 sleep 5
-python3 -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE} climate.csv=/monica_data/climate-data/climate-min.csv writenv=True
+python3 -u $MONICA_HOME/Examples/Hohenfinow2/python3/run-producer.py server=${LINKED_MONICA_SERVICE} climate.csv=/monica_data/climate-data/climate-min.csv writenv=True
 sleep 5
 cp $MONICA_HOME/testing/1.csv /out
 $MONICA_HOME/testing/CoreConsoleParser -ref $MONICA_HOME/testing/testreference.csv -totest $MONICA_HOME/testing/1.csv
