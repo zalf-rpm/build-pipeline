df0b38dcd052f1f1a4d3ca9dfeb1851a1414cd83
sanny <38938782+sannyschulz@users.noreply.github.com>
2020-05-15 13:01:51 +0200

    switch to debian 10 and python3, save output

--- docker/dotnet-producer-consumer/start_producer_consumer.sh
+++ docker/dotnet-producer-consumer/start_producer_consumer.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 env
-python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True out=$MONICA_HOME/testing/ &
-sleep 5
-python -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE}
-sleep 5
 mkdir -p /out
+python3 -u $MONICA_HOME/Examples/Hohenfinow2/python/run-consumer.py server=${LINKED_MONICA_SERVICE} leave_after_finished_run=True out=$MONICA_HOME/testing/ &
+sleep 5
+python3 -u $MONICA_HOME/Examples/Hohenfinow2/python/run-producer.py server=${LINKED_MONICA_SERVICE} climate.csv=/monica_data/climate-data/climate-min.csv writenv=True
+sleep 5
 cp $MONICA_HOME/testing/1.csv /out
 $MONICA_HOME/testing/CoreConsoleParser -ref $MONICA_HOME/testing/testreference.csv -totest $MONICA_HOME/testing/1.csv
 return $?
