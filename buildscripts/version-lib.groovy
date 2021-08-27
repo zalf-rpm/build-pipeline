5e2d6e2df6c1da1c9b0932c74441aed8bd20952b
sanny <38938782+sannyschulz@users.noreply.github.com>
2021-08-27 10:34:53 +0200

    fix push to git

--- buildscripts/version-lib.groovy
+++ buildscripts/version-lib.groovy
@@ -8,13 +8,13 @@ def commitVersionFileToGit(versionString, relVersionFilePath, reporsitory_Url, g
             def setAuthor = """git config --global user.name \"$authorName\" """
             def setEmail = """git config --global user.email $authorEmail """
             //old def pushToGit = """git push https://${GIT_USERNAME}:${GIT_PASSWORD}@$reporsitory_Url $branch"""
-            def pushToGit = '''git push https://${GITHUB_ACCESS_TOKEN}@github.com/''' + """$reporsitory_Url.git $branch"""
+            def pushToGit = """git push https://${GITHUB_ACCESS_TOKEN}@github.com/${reporsitory_Url}.git ${branch}"""
             if (isUnix())
             { 
                 sh setAuthor
                 sh setEmail
                 sh gitCommitCmd
-                sh pushToGit   
+                sh pushToGit 
             }
             else
             {
@@ -70,7 +70,7 @@ def createGitTag(versionString, message, reporsitory_Url, gitCheckoutFolder, cre
                 def setAuthor = """git config --global user.name \"$authorName\" """
                 def setEmail = """git config --global user.email $authorEmail """
                 //def pushToGit = """git push https://${GIT_USERNAME}:${GIT_PASSWORD}@$reporsitory_Url $branch"""
-                def pushToGit = '''git push https://${GITHUB_ACCESS_TOKEN}@github.com/''' + """$reporsitory_Url.git $branch"""
+                def pushToGit = """git push https://${GITHUB_ACCESS_TOKEN}@github.com/${reporsitory_Url}.git ${branch}"""
         
                 if (isUnix())
                 { 
