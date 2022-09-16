31c5db96bfbefd09113ffbe81626e9a3ecc7f1be
SannySchulz <38938782+sannyschulz@users.noreply.github.com>
2022-09-16 17:00:53 +0200

    fix build, add svn revision, change memory setting

--- docker/simplace-hpc/build.sh
+++ docker/simplace-hpc/build.sh
@@ -1,15 +1,32 @@
 
-#!/bin/bash
+#!/bin/bash +x
 
-# copy compiled simplace folder  (assuming it can be found in ~/simplace)
-cp -r ~/simplace/lapclient/console ./simplace_exe
+# requirements:
+# docker 
+# subversion
+# precompiled version of simplace
+
+PWD=$(pwd)
+WORKDIR=${1:-$PWD}
+SOURCEDIR=${2:-$HOME/simplace}
+
+# copy compiled simplace folder into build folder
+SIMPLACEEXE=$WORKDIR/simplace_exe
+rm -rf $SIMPLACEEXE
+cp -r $SOURCEDIR/lapclient/console $SIMPLACEEXE
 # remove windows executable
-cd simplace_exe
+cd $SIMPLACEEXE
+echo $(pwd)
+ls
 rm simplace.exe
 
-# optional set memory option to use more memory on a HPC node 
-# replace'-Xmx10g' with '-Xmx30g'
-sed -i 's/-Xmx10g/-Xmx30g/g' simplace
+# get simplace_run svn revision
+cd $SOURCEDIR/simplace_run
 
+SIMPLACR_RUN_VERSION=$(svnversion)
+echo "version:" $SIMPLACR_RUN_VERSION
+
+cd $WORKDIR
 # build docker image
-docker build -t zalfrpm/simplace-hpc:5.0 --no-cache --build-arg EXECUTABLE_SOURCE=simplace_exe .
+docker build -t zalfrpm/simplace-hpc:5.0-$SIMPLACR_RUN_VERSION --no-cache --build-arg EXECUTABLE_SOURCE=simplace_exe .
+
--- docker/simplace-hpc/test_simplace.sh
+++ docker/simplace-hpc/test_simplace.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
 
+# requirements:
+# docker
+# /simplace/SIMPLACE_WORK must contain the SimulationExperimentTemplate simulation 
+# from /simplace_run/simulation
+
 PWD=$(pwd)
 rootDir=${1:-$PWD}
 IMG=${2:-'zalfrpm/simplace-hpc:5.0'}
@@ -25,9 +30,10 @@ touch ${TEST_PATH_PROJECTS}/myproject.txt
 SOLUTION=SimulationExperimentTemplate/solution/Lintul5.sol.xml
 PROJECT=SimulationExperimentTemplate/project/Lintul5All.proj.xml
 
- docker run --rm \
+docker run --rm \
 --mount type=bind,source=${TEST_PATH_WORK},target=${SIMPLACE_WORKDIR} \
 --mount type=bind,source=${TEST_PATH_OUT},target=${OUTDIR} \
 --mount type=bind,source=${TEST_PATH_DATA},target=${DATADIR} \
 --mount type=bind,source=${TEST_PATH_PROJECTS},target=${PROJECTDIR} \
+--user $(id -u):$(id -g) \
 $IMG /simplace_start.sh $SOLUTION $PROJECT true 1 8 false
