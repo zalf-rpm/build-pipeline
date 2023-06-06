ab64562ecae71f833abd1338309f038f9866e79c
sanny <38938782+sannyschulz@users.noreply.github.com>
2023-06-06 18:44:00 +0200

    fixate python version, fix password reset

--- hpc/jupyter/lab/installjupyter_python3.9_1.0.sh
+++ hpc/jupyter/lab/installjupyter_python3.9_1.0.sh
@@ -11,7 +11,7 @@ ENVPATH=$WORKDIR/.conda/envs/jupyterenv
 
 # create conda environment if not already existing
 if [ ! -e ${ENVPATH} ] ; then
-conda create -y --name jupyterenv
+conda create -y --name jupyterenv python=3.9
 conda activate jupyterenv
 # install jupyterlab
 conda install -y -c conda-forge jupyterlab
@@ -25,6 +25,7 @@ jupyter server --generate-config
 fi
 # set(reset) password
 if [ ! -z "$PASSWORD" ] ; then
+conda activate jupyterenv
 HASH=$(python -c "exec(\"from jupyter_server.auth import passwd\nprint(passwd('$PASSWORD','sha1'))\")")
 sed -i "s/# c.ServerApp.password = .*/c.ServerApp.password = u'$HASH'/g" $WORKDIR/.jupyter/jupyter_server_config.py 
 fi
