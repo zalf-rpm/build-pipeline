7fa7c062ade1a23ea91225011ec364ce070b790d
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-02-14 19:16:48 +0100

    code server not yet working

---
+++ hpc/vscode-server/code_server_4.21.1.sh
@@ -0,0 +1,110 @@
+#!/bin/sh -x
+#SBATCH --time=08:00:00
+#SBATCH --signal=USR2
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=40
+#SBATCH --output=/home/%u/code-server.job.%j
+
+# code_server from https://github.com/linuxserver/docker-code-server
+
+SCRIPT_USER=$1
+LOGIN_HOST_NAME=$2
+MOUNT_PROJECT=$3
+MOUNT_DATA=$4
+HOMEDIR=$5
+SINGULARITY_IMAGE=$6
+MOUNT_DATA_SOURCE1=$7
+MOUNT_DATA_SOURCE2=$8
+MOUNT_DATA_SOURCE3=$9
+PASSW=${10}
+READ_ONLY_SOURCES=${11}
+
+# fail if no password is given
+if [ -z "$PASSW" ] ; then
+    echo "No password given"
+    exit 1
+fi
+# generate hashed password
+PYTHONIMG=/beegfs/common/singularity/jupyter/scipy-notebook_lab-3.4.2.sif
+HASH=$(singularity exec $PYTHONIMG python -c "exec(\"from jupyter_server.auth import passwd\nprint(passwd('$PASSW','sha1'))\")")
+
+mkdir -p ${HOMEDIR}/.config/code-server/
+# create config.yaml
+if [ ! -f ${HOMEDIR}/.config/code-server/config.yaml ] ; then
+
+cat <<EOF > ${HOMEDIR}/.config/code-server/config.yaml
+bind-addr: *:8443
+auth: password
+hashed-password: $HASH
+cert: false
+EOF
+elif [ ! -z "$PASSW" ] ; then
+# update password
+sed -i "s/hashed-password: .*/hashed-password: $HASH/g" ${HOMEDIR}/.config/code-server/config.yaml
+fi 
+
+PROJECT=/project
+DATA=/data
+CLUSTERHOME=/myhome
+MOUNT_CLUSTERHOME=/home/$SCRIPT_USER
+CONFIG=/config
+MOUNT_CONFIG=$HOMEDIR/.config
+mkdir -p $MOUNT_CONFIG
+
+# if MOUNT_DATA_SOURCEx is not none, add to MOUNT_DATA_SOURCES
+# if read_only_sources is true, mount data sources as read-only
+MOUNT_DATA_SOURCES=""
+if [ $MOUNT_DATA_SOURCE1 != "none" ] ; then
+  MOUNT_DATA_SOURCES="$MOUNT_DATA_SOURCES,$MOUNT_DATA_SOURCE1:$MOUNT_DATA_SOURCE1"
+  if [ $READ_ONLY_SOURCES == "true" ] ; then
+    MOUNT_DATA_SOURCES="$MOUNT_DATA_SOURCES:ro"
+  fi
+fi
+if [ $MOUNT_DATA_SOURCE2 != "none" ] ; then
+  MOUNT_DATA_SOURCES="$MOUNT_DATA_SOURCES,$MOUNT_DATA_SOURCE2:$MOUNT_DATA_SOURCE2"
+  if [ $READ_ONLY_SOURCES == "true" ] ; then
+    MOUNT_DATA_SOURCES="$MOUNT_DATA_SOURCES:ro"
+  fi
+fi
+if [ $MOUNT_DATA_SOURCE3 != "none" ] ; then
+  MOUNT_DATA_SOURCES="$MOUNT_DATA_SOURCES,$MOUNT_DATA_SOURCE3:$MOUNT_DATA_SOURCE3"
+  if [ $READ_ONLY_SOURCES == "true" ] ; then
+    MOUNT_DATA_SOURCES="$MOUNT_DATA_SOURCES:ro"
+  fi
+fi
+
+
+export SINGULARITY_HOME=${HOMEDIR}
+
+cd ${HOMEDIR}
+
+export SINGULARITYENV_DEFAULT_WORKSPACE=${HOMEDIR}
+export SINGULARITYENV_TZ=Etc/UTC
+#export SINGULARITYENV_PASSWORD=$PASSW
+export SINGULARITYENV_USER=$(id -un)
+
+cat 1>&2 <<END
+1. SSH tunnel from your workstation using the following command:
+
+   ssh -N -L 8443:${HOSTNAME}:8443 ${SCRIPT_USER}@${LOGIN_HOST_NAME}
+
+   and point your web browser to http://localhost:8443
+
+2. log in to Code Server using the following credentials:
+
+   user: ${SINGULARITYENV_USER}
+   password: your selected password 
+When done using Code Server, terminate the job by:
+
+1. Exit the Code Session ("File" -> "Exit" in the menu of the Code Server window)
+2. Issue the following command on the login node:
+
+      scancel -f ${SLURM_JOB_ID}
+END
+
+singularity exec --cleanenv \
+-B $MOUNT_CONFIG:$CONFIG,$MOUNT_PROJECT:$PROJECT,$MOUNT_DATA:$DATA:ro,$MOUNT_CLUSTERHOME:${CLUSTERHOME}${MOUNT_DATA_SOURCES} \
+-H $SINGULARITY_HOME \
+-W $SINGULARITY_HOME $SINGULARITY_IMAGE \
+/app/code-server/bin/code-server 
+printf 'code sever exited' 1>&2
---
+++ hpc/vscode-server/code_server_rundeck_hpc_user.sh
@@ -0,0 +1,128 @@
+#!/bin/bash -x
+#/ usage: start ?user? ?job_exec_id? ?host? ?mount_source1? ?mount_source2? ?mount_source3? ?estimated_time? ?partition? ?version? ?password? ?read_only_sources?
+set -eu
+[[ $# < 11 ]] && {
+  grep '^#/ usage:' <"$0" | cut -c4- >&2 ; exit 2;
+}
+
+echo "Set env"
+
+export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:~/.local/bin:~/bin
+
+USER=$1
+JOB_EXEC_ID=$2
+LOGIN_HOST=$3
+TIME=$4
+PARTITION=$5
+VERSION=$6
+PASSW=$7
+MOUNT_DATA_SOURCE1=$8 # e.g climate data
+MOUNT_DATA_SOURCE2=$9 # e.g. project data
+MOUNT_DATA_SOURCE3=${10} # e.g. other sources
+READ_ONLY_SOURCES=${11}
+
+#https://github.com/linuxserver/docker-code-server
+
+SBATCH_JOB_NAME="code-server${VERSION}"
+
+MOUNT_PROJECT=/beegfs/$USER/
+MOUNT_DATA=/beegfs/common/data
+WORKDIR=/beegfs/${USER}/code_server_playground${VERSION}
+LOGS=$WORKDIR/log
+
+mkdir -p -m 700 $WORKDIR
+mkdir -p -m 700 $LOGS
+
+#parse output of squeue for job name
+BATCHID=$(squeue --noheader -o "%i" -n $SBATCH_JOB_NAME -u $(whoami))
+
+# check if job is running
+if [ -z "$BATCHID" ] ; then
+
+  # fail if no password is given
+  if [ -z "$PASSW" ] ; then
+      echo "No password given"
+      exit 1
+  fi
+
+  # get code_server image from docker
+  IMAGE_DIR=/beegfs/common/singularity/code_server
+  SINGULARITY_IMAGE=code-server_${VERSION}.sif
+  IMAGE_CODE_SERVER_PATH=${IMAGE_DIR}/${SINGULARITY_IMAGE}
+
+  # check if image exists
+
+  if [ ! -e ${IMAGE_CODE_SERVER_PATH} ] ; then
+  echo "File '${IMAGE_CODE_SERVER_PATH}' not found"
+  exit 1
+  fi
+
+  HPC_PARTITION="--partition=compute"
+  CORES=80
+  if [ $PARTITION == "highmem" ] ; then 
+    HPC_PARTITION="--partition=highmem"
+    CORES=80
+  elif [ $PARTITION == "gpu" ] ; then 
+    HPC_PARTITION="--partition=gpu"
+    CORES=48
+  elif [ $PARTITION == "fat" ] ; then 
+    HPC_PARTITION="--partition=fat"
+    CORES=160
+  fi
+
+  if [ ! -d ${MOUNT_DATA_SOURCE1} ] ; then
+  echo "Directory '${MOUNT_DATA_SOURCE1}' not found"
+  MOUNT_DATA_SOURCE1=none
+  fi
+  if [ ! -d ${MOUNT_DATA_SOURCE2} ] ; then
+  echo "Directory '${MOUNT_DATA_SOURCE2}' not found"
+  MOUNT_DATA_SOURCE2=none
+  fi
+  if [ ! -d ${MOUNT_DATA_SOURCE3} ] ; then
+  echo "Directory '${MOUNT_DATA_SOURCE3}' not found"
+  MOUNT_DATA_SOURCE3=none
+  fi
+ # 
+
+
+
+  # required nodes 1
+  CMD_LINE_SLURM="--parsable --job-name=${SBATCH_JOB_NAME} ${HPC_PARTITION} --time=${TIME} -N 1 -c $CORES -o ${LOGS}/code-server_${USER}_%j.log"
+  SCRIPT_INPUT="${USER} ${LOGIN_HOST} ${MOUNT_PROJECT} ${MOUNT_DATA} ${WORKDIR} ${IMAGE_CODE_SERVER_PATH} ${MOUNT_DATA_SOURCE1} ${MOUNT_DATA_SOURCE2} ${MOUNT_DATA_SOURCE3} ${PASSW} ${READ_ONLY_SOURCES}"
+
+  cd ${WORKDIR}
+  BATCHID=$( sbatch $CMD_LINE_SLURM /beegfs/common/batch/code_server_${VERSION}.sh $SCRIPT_INPUT )
+
+  LOG_NAME=${LOGS}/code-server_${USER}_${BATCHID}.log
+  COUNTER=0
+  while [ ! -f ${LOG_NAME} ] && [ ! $COUNTER -eq 30 ] ; do 
+  sleep 10
+  COUNTER=$(($COUNTER + 1))
+  if [ $COUNTER == 30 ] ; then
+      scancel $BATCHID
+      echo "timeout: no free slot available. Try again later"
+  fi 
+  done
+  sleep 5
+
+else 
+  echo "Job is already running"
+
+
+  NODE=$(squeue --noheader -o "%R" -n $SBATCH_JOB_NAME -u $(whoami) )
+  TIME=$(squeue --noheader -o "%.10M" -n $SBATCH_JOB_NAME -u $(whoami) )
+  TIMESPAN=$(squeue --noheader -o "%.9l" -n $SBATCH_JOB_NAME -u $(whoami) )
+
+  echo "Job ID: $BATCHID"
+  echo "Node: $NODE"
+  echo "Running since: $TIME"
+  echo "Total time span: $TIMESPAN"
+  
+  LOG_NAME=${LOGS}/code-server_${USER}_${BATCHID}.log
+  echo "Log file: ${LOG_NAME}"
+fi
+
+if [ -f ${LOG_NAME} ] ; then
+    cat ${LOG_NAME}
+fi 
+
