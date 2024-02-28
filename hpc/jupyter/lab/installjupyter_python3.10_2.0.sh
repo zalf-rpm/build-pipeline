8b6b1ab72d631cc4664c9f1330c21cbd08972b10
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-02-28 16:58:07 +0100

    reverted

--- hpc/jupyter/lab/installjupyter_python3.10_2.0.sh
+++ hpc/jupyter/lab/installjupyter_python3.10_2.0.sh
@@ -1,29 +1,9 @@
 #!/bin/bash 
 
 WORKDIR=$1
+PASSWORD=$2
+
 cd $WORKDIR
-
-# read password from file
-TRANS=${WORKDIR}/.rundeck/jupyter_trans.yml
-# check if setup file exists
-if [ ! -f ${TRANS} ] ; then
-    echo "setup file not found"
-    exit 1
-fi
-# get the second argument from the setup file
-PASSWORD=$( cat $TRANS | cut -d' ' -f2)
-
-# strip leading and trailing whitespaces
-PASSWORD=$( echo $PASSWORD | xargs )
-
-# remove setup file 
-rm -f $TRANS
-
-# fail if no password is given
-if [ -z "$PASSWORD" ] ; then
-    echo "No password given"
-    exit 1
-fi
 
 source /opt/conda/etc/profile.d/conda.sh
 
@@ -33,26 +13,8 @@ ENVPATH=$WORKDIR/.conda/envs/jupyterenv
 if [ ! -e ${ENVPATH} ] ; then
 conda create -y --name jupyterenv python=3.10
 conda activate jupyterenv
-# install jupyterlab and other packages
-conda clean -y --index-cache
+# install jupyterlab
 conda install -y -c conda-forge jupyterlab
-conda clean -y --index-cache
-conda install -y -c conda-forge ipywidgets
-conda clean -y --index-cache
-conda install -y -c conda-forge widgetsnbextension
-conda clean -y --index-cache
-conda install -y -c conda-forge matplotlib-base
-conda clean -y --index-cache
-conda install -y -c conda-forge pandas
-conda clean -y --index-cache
-conda install -y -c conda-forge scipy
-conda clean -y --index-cache
-conda install -y -c conda-forge seaborn
-conda clean -y --index-cache
-conda install -y -c conda-forge jupyterlab-git
-conda clean -y --index-cache
-conda install -y -c conda-forge jupyterlab-drawio
-
 conda clean -y --all
 fi
 # create jupyter config if not already existing
@@ -60,7 +22,6 @@ JUPYTER_PATH=$WORKDIR/.jupyter/jupyter_server_config.py
 if [ ! -e ${JUPYTER_PATH} ] ; then
 conda activate jupyterenv
 jupyter server --generate-config
-
 echo "set password hash"
 HASH=$(python -c "exec(\"from jupyter_server.auth import passwd\nprint(passwd('$PASSWORD','sha1'))\")")
 sed -i "s/# c.ServerApp.password = .*/c.PasswordIdentityProvider.hashed_password = u'$HASH'/g" $WORKDIR/.jupyter/jupyter_server_config.py 
