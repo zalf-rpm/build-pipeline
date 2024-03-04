8e607b7490ba86e66859e00128a14328157225f7
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-03-04 17:59:55 +0100

    update install scripts for hash

--- hpc/jupyter/lab/installjupyter_python3.10_2.0.sh
+++ hpc/jupyter/lab/installjupyter_python3.10_2.0.sh
@@ -1,9 +1,33 @@
 #!/bin/bash 
 
 WORKDIR=$1
-PASSWORD=$2
-
+READ_HASH=$2
 cd $WORKDIR
+
+HASH=""
+# check if READ_HASH is true
+if [ $READ_HASH == "true" ] ; then
+    # read hash from file
+    TRANS=${WORKDIR}/.rundeck/jupyter_trans.yml
+    # check if setup file exists
+    if [ ! -f ${TRANS} ] ; then
+        echo "setup file not found"
+        exit 1
+    fi
+    HASH=$( cat $TRANS)
+
+    # strip leading and trailing whitespaces
+    HASH=$( echo $HASH | xargs )
+
+    # remove setup file 
+    rm -f $TRANS
+
+    # fail if no HASH is given
+    if [ -z "$HASH" ] ; then
+        echo "No HASH given"
+        exit 1
+    fi
+fi
 
 source /opt/conda/etc/profile.d/conda.sh
 
@@ -13,8 +37,26 @@ ENVPATH=$WORKDIR/.conda/envs/jupyterenv
 if [ ! -e ${ENVPATH} ] ; then
 conda create -y --name jupyterenv python=3.10
 conda activate jupyterenv
-# install jupyterlab
+# install jupyterlab and other packages
+conda clean -y --index-cache
 conda install -y -c conda-forge jupyterlab
+conda clean -y --index-cache
+conda install -y -c conda-forge ipywidgets
+conda clean -y --index-cache
+conda install -y -c conda-forge widgetsnbextension
+conda clean -y --index-cache
+conda install -y -c conda-forge matplotlib-base
+conda clean -y --index-cache
+conda install -y -c conda-forge pandas
+conda clean -y --index-cache
+conda install -y -c conda-forge scipy
+conda clean -y --index-cache
+conda install -y -c conda-forge seaborn
+conda clean -y --index-cache
+conda install -y -c conda-forge jupyterlab-git
+conda clean -y --index-cache
+conda install -y -c conda-forge jupyterlab-drawio
+
 conda clean -y --all
 fi
 # create jupyter config if not already existing
@@ -22,15 +64,14 @@ JUPYTER_PATH=$WORKDIR/.jupyter/jupyter_server_config.py
 if [ ! -e ${JUPYTER_PATH} ] ; then
 conda activate jupyterenv
 jupyter server --generate-config
-echo "set password hash"
-HASH=$(python -c "exec(\"from jupyter_server.auth import passwd\nprint(passwd('$PASSWORD','sha1'))\")")
+
+# initialize password hash
+if [ -z "$HASH" ] ; then
+ # fail if no hash is given
+    echo "No hash given"
+    exit 1
+fi
+
 sed -i "s/# c.ServerApp.password = .*/c.PasswordIdentityProvider.hashed_password = u'$HASH'/g" $WORKDIR/.jupyter/jupyter_server_config.py 
 sed -i "s/# c.ServerApp.password_required = .*/c.PasswordIdentityProvider.password_required = True/g" $WORKDIR/.jupyter/jupyter_server_config.py 
-else
-
-# set(reset) password after installation
-echo "update password hash"
-conda activate jupyterenv
-HASH=$(python -c "exec(\"from jupyter_server.auth import passwd\nprint(passwd('$PASSWORD','sha1'))\")")
-sed -i "s/c.PasswordIdentityProvider.hashed_password = .*/c.PasswordIdentityProvider.hashed_password = u'$HASH'/g" $WORKDIR/.jupyter/jupyter_server_config.py 
 fi
--- hpc/jupyter/lab/installjupyter_python3.9_2.0.sh
+++ hpc/jupyter/lab/installjupyter_python3.9_2.0.sh
@@ -1,9 +1,33 @@
 #!/bin/bash 
 
 WORKDIR=$1
-PASSWORD=$2
-
+READ_HASH=$2
 cd $WORKDIR
+
+HASH=""
+# check if READ_HASH is true
+if [ $READ_HASH == "true" ] ; then
+    # read hash from file
+    TRANS=${WORKDIR}/.rundeck/jupyter_trans.yml
+    # check if setup file exists
+    if [ ! -f ${TRANS} ] ; then
+        echo "setup file not found"
+        exit 1
+    fi
+    HASH=$( cat $TRANS)
+
+    # strip leading and trailing whitespaces
+    HASH=$( echo $HASH | xargs )
+
+    # remove setup file 
+    rm -f $TRANS
+
+    # fail if no HASH is given
+    if [ -z "$HASH" ] ; then
+        echo "No HASH given"
+        exit 1
+    fi
+fi
 
 source /opt/conda/etc/profile.d/conda.sh
 
@@ -31,15 +55,14 @@ JUPYTER_PATH=$WORKDIR/.jupyter/jupyter_server_config.py
 if [ ! -e ${JUPYTER_PATH} ] ; then
 conda activate jupyterenv
 jupyter server --generate-config
-echo "set password hash"
-HASH=$(python -c "exec(\"from jupyter_server.auth import passwd\nprint(passwd('$PASSWORD','sha1'))\")")
+
+# initialize password hash
+if [ -z "$HASH" ] ; then
+ # fail if no hash is given
+    echo "No hash given"
+    exit 1
+fi
+
 sed -i "s/# c.ServerApp.password = .*/c.PasswordIdentityProvider.hashed_password = u'$HASH'/g" $WORKDIR/.jupyter/jupyter_server_config.py 
 sed -i "s/# c.ServerApp.password_required = .*/c.PasswordIdentityProvider.password_required = True/g" $WORKDIR/.jupyter/jupyter_server_config.py 
-else
-
-# set(reset) password after installation
-echo "update password hash"
-conda activate jupyterenv
-HASH=$(python -c "exec(\"from jupyter_server.auth import passwd\nprint(passwd('$PASSWORD','sha1'))\")")
-sed -i "s/c.PasswordIdentityProvider.hashed_password = .*/c.PasswordIdentityProvider.hashed_password = u'$HASH'/g" $WORKDIR/.jupyter/jupyter_server_config.py 
 fi
