e8c1d21e820b36ace5aeb2555acff33cb9a188fd
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-07-29 18:03:25 +0200

    direct rscript execution

---
+++ hpc/r-studio/myrstudio/myrscript_rundeck_hpc_user.sh
@@ -0,0 +1,77 @@
+#!/bin/bash -x
+#/ usage: start ?user? ?job_exec_id? ?host? ?mount_source1? ?mount_source2? ?mount_source3? ?estimated_time? ?partition? ?version? ?read_only_sources?
+set -eu
+[[ $# < 9 ]] && {
+  grep '^#/ usage:' <"$0" | cut -c4- >&2 ; exit 2;
+}
+
+echo "Set env"
+
+export PATH=$PATH:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:~/.local/bin:~/bin
+
+USER=$1
+TIME=$2
+PARTITION=$3
+VERSION=$4
+MOUNT_DATA_SOURCE1=$5 # e.g climate data
+MOUNT_DATA_SOURCE2=$6 # e.g. project data
+MOUNT_DATA_SOURCE3=${7} # e.g. other sources
+READ_ONLY_SOURCES=${8}
+SCRIPT_NAME=$9
+PARAMS=${@:10}
+
+SBATCH_JOB_NAME="R_script_playground"
+
+MOUNT_PROJECT=/beegfs/$USER/
+MOUNT_DATA=/beegfs/common/data
+WORKDIR=/beegfs/${USER}/R_playground${VERSION}
+LOGS=$WORKDIR/log
+
+mkdir -p -m 700 $WORKDIR
+mkdir -p -m 700 $LOGS
+
+# get r-studio image from docker
+IMAGE_DIR=/beegfs/common/singularity/R
+SINGULARITY_IMAGE=rstudio_dev_${VERSION}.sif
+IMAGE_RSTUDIO_PATH=${IMAGE_DIR}/${SINGULARITY_IMAGE}
+
+if [ ! -e ${IMAGE_RSTUDIO_PATH} ] ; then
+echo "File '${IMAGE_RSTUDIO_PATH}' not found"
+fi
+
+HPC_PARTITION="--partition=compute"
+CORES=80
+if [ $PARTITION == "highmem" ] ; then 
+  HPC_PARTITION="--partition=highmem"
+  CORES=80
+elif [ $PARTITION == "gpu" ] ; then 
+  HPC_PARTITION="--partition=gpu"
+  CORES=48
+elif [ $PARTITION == "fat" ] ; then 
+  HPC_PARTITION="--partition=fat"
+  CORES=160
+fi
+
+if [ ! -d ${MOUNT_DATA_SOURCE1} ] ; then
+echo "Directory '${MOUNT_DATA_SOURCE1}' not found"
+MOUNT_DATA_SOURCE1=none
+fi
+if [ ! -d ${MOUNT_DATA_SOURCE2} ] ; then
+echo "Directory '${MOUNT_DATA_SOURCE2}' not found"
+MOUNT_DATA_SOURCE2=none
+fi
+if [ ! -d ${MOUNT_DATA_SOURCE3} ] ; then
+echo "Directory '${MOUNT_DATA_SOURCE3}' not found"
+MOUNT_DATA_SOURCE3=none
+fi
+
+# required nodes 1
+CMD_LINE_SLURM="--parsable --job-name=${SBATCH_JOB_NAME} ${HPC_PARTITION} --time=${TIME} -N 1 -c $CORES -o ${LOGS}/rscript_${USER}_%j.log"
+SCRIPT_INPUT="${USER} ${MOUNT_PROJECT} ${MOUNT_DATA} ${WORKDIR} ${IMAGE_RSTUDIO_PATH} ${MOUNT_DATA_SOURCE1} ${MOUNT_DATA_SOURCE2} ${MOUNT_DATA_SOURCE3} ${READ_ONLY_SOURCES}"
+SCRIPT_INPUT="${SCRIPT_INPUT} ${SCRIPT_NAME} ${PARAMS}"
+
+cd ${WORKDIR}
+BATCHID=$( sbatch $CMD_LINE_SLURM /beegfs/common/batch/r_script4.2.sh $SCRIPT_INPUT )
+
+
+
---
+++ hpc/r-studio/myrstudio/r_test_script4.2.R
@@ -0,0 +1,27 @@
+# load a system library
+library(ggplot2)
+
+# print path to user libraries
+libpath <- .libPaths()
+print(libpath)
+
+# load a user library
+library(this.path)
+
+# print current working directory
+current_dir <- getwd()
+print(current_dir)
+
+# create a data frame
+data <- data.frame(
+    name = c("A", "B", "C", "D"),
+    value = c(3, 12, 5, 18)
+)
+
+# create a bar plot
+p <- ggplot(data, aes(x = name, y = value)) +
+    geom_bar(stat = "identity")
+
+# save the plot
+ggsave("plot4.2.png", plot = p, device = "png")
+
