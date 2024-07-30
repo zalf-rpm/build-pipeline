7c31505c10d6c37e2789eeee7306c35366fb359a
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-07-30 12:06:48 +0200

    test directory mounts

--- hpc/r-studio/myrstudio/r_test_script4.2.R
+++ hpc/r-studio/myrstudio/r_test_script4.2.R
@@ -1,3 +1,13 @@
+# Purpose: R script to test RScript on HPC on a RStudio playground
+# it requires that the playground has been started at least once before
+# with the RStudio image
+# Other requirements:
+# this.path as a user library
+# mounted /myhome, /project, /data
+# optional: some command line arguments
+# optional: /scratch, /beegfs/common/singularity, /beegfs/common/batch
+
+
 # load a system library
 library(ggplot2)
 
@@ -31,3 +41,29 @@ p <- ggplot(data, aes(x = name, y = value)) +
 # save the plot
 ggsave("plot4.2.png", plot = p, device = "png")
 
+# list files in mounted directories
+# home directory
+print("Home directory:")
+print(list.files("/myhome"))
+# project directory
+print("Project directory:")
+print(list.files("/project"))
+# data directory
+print("Data directory:")
+print(list.files("/data"))
+# if mounted
+if (file.exists("/scratch")) {
+    # scratch directory
+    print("Scratch directory:")
+    print(list.files("/scratch"))
+}
+if (file.exists("/beegfs/common/singularity")) {
+    # beegfs directory
+    print("Beegfs singularity directory:")
+    print(list.files("/beegfs/common/singularity"))
+}
+if (file.exists("/beegfs/common/batch")) {
+    # beegfs directory
+    print("Beegfs batch directory:")
+    print(list.files("/beegfs/common/batch"))
+}
