d77f734f12753d5c90ded933a8c7f8b58b00ea51
sanny <38938782+sannyschulz@users.noreply.github.com>
2024-10-16 18:40:03 +0200

    fix install

--- hpc/jupyter/lab/installjupyter_noconda_p3.10_5.sh
+++ hpc/jupyter/lab/installjupyter_noconda_p3.10_5.sh
@@ -2,6 +2,9 @@
 
 WORKDIR=$1
 READ_HASH=$2
+# setup python env for ai kernels, (optional, default is false)
+SETUP_AI_ENV_1=${3:-false}
+
 cd $WORKDIR
 
 HASH=""
@@ -35,10 +38,8 @@ if [ ! -e ${ENVPATH} ] ; then
     pipx inject jupyter jupyterlab-git jupyterlab-drawio matplotlib pandas ipykernel ipyparallel jupyterlab-nvdashboard
     pipx install poetry
     pipx ensurepath
-
-
-
 fi
+
 # make sure jupyter is in PATH
 export PATH=$WORKDIR/.local/bin:$PATH
 
@@ -47,82 +48,105 @@ export PATH=$WORKDIR/.local/bin:$PATH
 # create env for each python version
 
 if [ ! -e $WORKDIR/.envs/python3.7 ] ; then
+    echo "Creating python3.7 kernel"
 
     python3.7 -m venv $WORKDIR/.envs/python3.7
     source $WORKDIR/.envs/python3.7/bin/activate
-    pip install ipykernel
+    pip install ipykernel ipyparallel
     python3.7 -m ipykernel install --user --name python3.7 --display-name "Python 3.7"
     deactivate
 fi 
 
 if [ ! -e $WORKDIR/.envs/python3.8 ] ; then
+    echo "Creating python3.8 kernel"
 
     python3.8 -m venv $WORKDIR/.envs/python3.8
     source $WORKDIR/.envs/python3.8/bin/activate
-    pip install ipykernel
+    pip install ipykernel ipyparallel
     python3.8 -m ipykernel install --user --name python3.8 --display-name "Python 3.8"
     deactivate
 fi
 
 if [ ! -e $WORKDIR/.envs/python3.9 ] ; then
+    echo "Creating python3.9 kernel"
 
     python3.9 -m venv $WORKDIR/.envs/python3.9
     source $WORKDIR/.envs/python3.9/bin/activate
-    pip install ipykernel
+    pip install ipykernel ipyparallel
     python3.9 -m ipykernel install --user --name python3.9 --display-name "Python 3.9"
     deactivate
 fi
 
-if [ ! -e $WORKDIR/.envs/python3.10 ] ; then
-
-    python3.10 -m venv $WORKDIR/.envs/python3.10
-    source $WORKDIR/.envs/python3.10/bin/activate
-    pip install ipykernel
-    python3.10 -m ipykernel install --user --name python3.10 --display-name "Python 3.10"
-    deactivate
-fi
-
 if [ ! -e $WORKDIR/.envs/python3.11 ] ; then
+    echo "Creating python3.11 kernel"
 
     python3.11 -m venv $WORKDIR/.envs/python3.11
     source $WORKDIR/.envs/python3.11/bin/activate
-    pip install ipykernel
+    pip install ipykernel ipyparallel
     python3.11 -m ipykernel install --user --name python3.11 --display-name "Python 3.11"
     deactivate
 fi
 
 if [ ! -e $WORKDIR/.envs/python3.12 ] ; then
+    echo "Creating python3.12 kernel"
 
     python3.12 -m venv $WORKDIR/.envs/python3.12
     source $WORKDIR/.envs/python3.12/bin/activate
-    pip install ipykernel
+    pip install ipykernel ipyparallel
     python3.12 -m ipykernel install --user --name python3.12 --display-name "Python 3.12"
     deactivate
 fi
 
 if [ ! -e $WORKDIR/.envs/python3.13 ] ; then
+    echo "Creating python3.13 kernel"
 
     python3.13 -m venv $WORKDIR/.envs/python3.13
     source $WORKDIR/.envs/python3.13/bin/activate
-    pip install ipykernel
+    pip install ipykernel ipyparallel
     python3.13 -m ipykernel install --user --name python3.13 --display-name "Python 3.13"
     deactivate
 fi
 
-# AI kernels
+if [ ! -e $WORKDIR/.envs/python3.10_default ] ; then
 
-# pytorch 
-
-if [ ! -e $WORKDIR/.envs/pytorch ] ; then
-
-    python3.12 -m venv $WORKDIR/.envs/pytorchcu121
-    source $WORKDIR/.envs/pytorch/bin/activate
-    pip install ipykernel torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
-    python3.12 -m ipykernel install --user --name pytorchcu121 --display-name "PyTorch CUDA 12.1"
+    python3.10 -m venv $WORKDIR/.envs/python3.10_default 
+    source $WORKDIR/.envs/python3.10_default/bin/activate
+    pip install ipykernel matplotlib seaborn numpy pandas scipy ipyparallel
+    python3.10 -m ipykernel install --user --name python3.10_default --display-name "Python 3.10 Default"
     deactivate
+
 fi
 
+# AI kernel
+if [[ $SETUP_AI_ENV_1 == "true" && ! -e $WORKDIR/.envs/pytorchcu121 ]] ; then
 
+    python3.10 -m venv $WORKDIR/.envs/pytorchcu121
+    source $WORKDIR/.envs/pytorchcu121/bin/activate
+    echo "Installing ipykernel"
+    pip install ipykernel ipyparallel
+
+    echo "Installing GDAL"
+    pip install GDAL==3.6.4
+
+    echo "Installing pytorch packages"
+    pip install torch torchvision torchaudio torchmetrics
+
+    echo "Installing other deep learning packages"
+    pip install pillow lightly lightning captum albumentations 
+
+    echo "Installing Hyperparameter optimization packages"
+    pip install ray bayesian-optimization hpbandster
+
+    echo "Installing other Geospatial packages"
+    pip install geopandas geos geotiff shapely rasterio fiona cartopy
+
+    echo "Installing other packages"
+    pip install matplotlib seaborn numpy pandas scikit-image scikit-learn scipy memory-profiler pyzmq netcdf4 nco
+
+    echo "adding kernel to jupyter"
+    python3.10 -m ipykernel install --user --name pytorchcu121 --display-name "PyTorch CUDA 12.1"
+    deactivate
+fi
 
 # create jupyter config if not already existing
 JUPYTER_PATH=$WORKDIR/.jupyter/jupyter_server_config.py
@@ -138,4 +162,14 @@ fi
 
 sed -i "s/# c.ServerApp.password = .*/c.PasswordIdentityProvider.hashed_password = u'$HASH'/g" $WORKDIR/.jupyter/jupyter_server_config.py 
 sed -i "s/# c.ServerApp.password_required = .*/c.PasswordIdentityProvider.password_required = True/g" $WORKDIR/.jupyter/jupyter_server_config.py 
+
+# set default ipykernel from pyton3 to python3.12_default
+# c.MultiKernelManager.default_kernel_name = 'python3'
+# set AI kernel as default if SETUP_AI_ENV_1 is true
+if [ $SETUP_AI_ENV_1 == "true" ] ; then
+    sed -i "s/# c.MultiKernelManager.default_kernel_name = .*/c.MultiKernelManager.default_kernel_name = 'pytorchcu121'/g" $WORKDIR/.jupyter/jupyter_server_config.py
+else
+    sed -i "s/# c.MultiKernelManager.default_kernel_name = .*/c.MultiKernelManager.default_kernel_name = 'python3.10_default'/g" $WORKDIR/.jupyter/jupyter_server_config.py
+fi
+
 fi
