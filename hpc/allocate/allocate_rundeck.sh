3f59a79de4d01898db8e1e95f83445c2c5107e2e
sanny <38938782+sannyschulz@users.noreply.github.com>
2026-04-20 17:43:01 +0200

    fix: update resource allocation logic in rundeck script for improved partition handling

--- hpc/allocate/allocate_rundeck.sh
+++ hpc/allocate/allocate_rundeck.sh
@@ -13,18 +13,44 @@ sinfo
 
 
 TIME=$1
-PARTITION=$2
+SELECTION=$2
 
-HPC_PARTITION="--partition=compute"
-if [ $PARTITION == "highmem" ] ; then 
-    HPC_PARTITION="--partition=highmem"
-elif [ $PARTITION == "gpu-Nvidia-Tesla-V100" ] ; then 
-    HPC_PARTITION="--partition=gpu -x gpu005"
-elif [ $PARTITION == "gpu-Nvidia-Tensor-Core-H100" ] ; then 
-    HPC_PARTITION="--partition=gpu -x gpu001,gpu002,gpu003,gpu004"
-elif [ $PARTITION == "fat" ] ; then 
-    HPC_PARTITION="--partition=fat"
+RESOURCE_REQUEST="-c 2 --mem=1G --partition=compute,highmem"
+
+if [ $SELECTION == "compute-exclusive" ] ; then 
+    RESOURCE_REQUEST="--partition=compute --exclusive"
+elif [ $SELECTION == "highmem-exclusive" ] ; then 
+    RESOURCE_REQUEST="--partition=highmem --exclusive"
+elif [ $SELECTION == "gpu-Nvidia-Tesla-V100" ] ; then 
+    RESOURCE_REQUEST="--partition=gpu -x gpu005 --exclusive"
+elif [ $SELECTION == "gpu-Nvidia-Tensor-Core-H100" ] ; then 
+    RESOURCE_REQUEST="--partition=gpu -x gpu001,gpu002,gpu003,gpu004 --exclusive"
+elif [ $SELECTION == "fat-exclusive" ] ; then 
+    RESOURCE_REQUEST="--partition=fat --exclusive"
+elif [ $SELECTION == "2vCPU-2GB-Ram" ] ; then 
+    RESOURCE_REQUEST="-c 2 --mem=2G"
+elif [ $SELECTION == "4vCPU-16GB-Ram" ] ; then 
+    RESOURCE_REQUEST="-c 4 --mem=16G"
+elif [ $SELECTION == "16vCPU-32GB-Ram" ] ; then 
+    RESOURCE_REQUEST="-c 16 --mem=32G"    
+elif [ $SELECTION == "40vCPU-40GB-Ram" ] ; then 
+    RESOURCE_REQUEST="-c 40 --mem=40G"
+
+else
+    echo "Error: Unknown selection: $SELECTION" >&2
+    exit 1
 fi
+# summary
+# compute-exclusive: ~93GB RAM, 80 vCores, exclusive access
+# highmem-exclusive: ~188GB RAM, 80 vCores, exclusive access
+# gpu-Nvidia-Tesla-V100: 2xV100 GPU, ~93GB RAM, 48 vCores, exclusive access
+# gpu-Nvidia-Tensor-Core-H100: 4xH100 GPU, ~756GB RAM, 128 vCores, exclusive access
+# fat-exclusive: ~1.5TB RAM, 160 vCores, exclusive access
+# 2vCPU-2GB-Ram: 2 vCPU and 2 GB RAM, no exclusive access
+# 4vCPU-16GB-Ram: 4 vCPU and 16 GB RAM, no exclusive access
+# 16vCPU-32GB-Ram: 16 vCPU and 32 GB RAM, no exclusive access
+# 40vCPU-40GB-Ram: 40 vCPU and 40 GB RAM, no exclusive access
+
 
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
 WORKDIR=/home/$USER/logs/allocate
@@ -33,7 +59,7 @@ LOGFILE=$WORKDIR/$DATE-out.log
 
 # salloc -N 1 --exclusive --immediate=10 --partition=compute --time=00:05:00 --no-shell > /home/user/logs/allocate/$(date +%Y-%m-%d-%H-%M-%S)-out.log 2>&1 &
 # allocate node and redirect standard output and error to log file 
-salloc -N 1 --exclusive --immediate=10 $HPC_PARTITION --time=$TIME --no-shell > $LOGFILE 2>&1
+salloc -N 1 --immediate=10 $RESOURCE_REQUEST --time=$TIME --no-shell > $LOGFILE 2>&1
 
 # read the log file to get the node name
 # Example log output:
