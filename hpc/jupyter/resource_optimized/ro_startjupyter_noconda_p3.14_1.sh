48bdf2f40fe8f35f36c8715b93eb357bce51629e
sanny <38938782+sannyschulz@users.noreply.github.com>
2026-05-29 18:54:16 +0200

    jupyter setup for python3.14/ubuntu26.04

---
+++ hpc/jupyter/resource_optimized/ro_startjupyter_noconda_p3.14_1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+
+WORKDIR=$1
+PORT=$2
+
+echo "Starting jupyter lab in ${WORKDIR} on port ${PORT}"
+
+cd $WORKDIR
+
+# make sure jupyter is in PATH
+export PATH=$WORKDIR/.local/bin:$PATH
+
+JUPYTERLAB_WORKSPACES_DIR=$WORKDIR
+
+jupyter lab --no-browser --ip "*" --port $PORT 
